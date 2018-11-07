652239df09cae607fcaa17213b62463bb8a48774
Neuromancer42 <chenyifan_1997@hotmail.com>
2018-11-07 14:38:01 +0800

    init with CreateClass

--- src/FetchClass.java
+++ src/FetchClass.java
@@ -1,9 +1,7 @@
-import soot.PackManager;
-import soot.Scene;
-import soot.SceneTransformer;
-import soot.Transform;
+import soot.*;
+import soot.util.*;
 
-import java.util.Map;
+import java.util.*;
 
 public class FetchClass {
     public static void main(String[] args) {
@@ -12,6 +10,20 @@ public class FetchClass {
                     protected void internalTransform(String phaseName,
                                                      Map options) {
                         System.err.println(Scene.v().getApplicationClasses());
+                        System.err.println(Scene.v().getApplicationClasses().
+                                getFirst().getMethodByName("main"));
+                        Chain<SootClass> sClasses = Scene.v().getApplicationClasses();
+                        Iterator classIt = sClasses.iterator();
+                        while (classIt.hasNext()) {
+                            SootClass sClass = (SootClass) classIt.next();
+                            System.err.println(sClass.getName());
+                            Chain<SootField> sFields = sClass.getFields();
+                            Iterator fieldIt = sFields.iterator();
+                            while (fieldIt.hasNext()) {
+                                SootField sField = (SootField) fieldIt.next();
+                                System.err.println(sField.getSignature());
+                            }
+                        }
                     }
                 }));
         soot.Main.main(args);
